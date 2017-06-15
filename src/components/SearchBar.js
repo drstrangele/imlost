9edfe9f4de8bb75ebf6cea57c4206d13c982eb42
drstrangele <chickenle@gmail.com>
2017-06-15 00:10:18 -0700

    added onclick to search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,15 +6,13 @@ class SearchBar extends Component {
         this.state = { term: '' }
     }
 
-    onInputChange(term) {
-        this.setState({term});
-        this.props.onTermChange(term);
-    }
-
     render() {
+      const { onSearch } = this.props;
+      const { term } = this.state;
         return (
             <div className="search">
-                <input onChange={event => this.onInputChange(event.target.value)} />
+                <input onChange={event => this.setState({ term: event.target.value })} />
+                <button onClick={() => onSearch(term)}>Search some giphys</button>
             </div>
         );
     }
